475e34c6eb535c810298bc8da452b1c6ae6127f8
Thinh Phan <21130548@st.hcmuaf.edu.vn>
2024-07-20 16:50:06 +0700

    entities ProductAppointment

--- Cua_hang_xe_may/src/main/java/com/demo/entities/ProductApointment.java
+++ Cua_hang_xe_may/src/main/java/com/demo/entities/ProductApointment.java
@@ -1,5 +1,45 @@
 package com.demo.entities;
 
 public class ProductApointment {
-
+	private int id;
+	private ProductColor productColor;
+	private int quantity;
+	private int appointmentId;
+	public ProductApointment(int id, ProductColor productColor, int quantity, int appointmentId) {
+		super();
+		this.id = id;
+		this.productColor = productColor;
+		this.quantity = quantity;
+		this.appointmentId = appointmentId;
+	}
+	public ProductApointment() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public ProductColor getProductColor() {
+		return productColor;
+	}
+	public void setProductColor(ProductColor productColor) {
+		this.productColor = productColor;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getAppointmentId() {
+		return appointmentId;
+	}
+	public void setAppointmentId(int appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+	
+	
 }
